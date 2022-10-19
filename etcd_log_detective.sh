602809298d808be09e18c48c9a9c5d075cef9598
peter daemonna ducai <peter.ducai@gmail.com>
2022-10-19 13:12:54 +0200

    keepalived

--- etcd_log_detective.sh
+++ etcd_log_detective.sh
@@ -24,9 +24,10 @@ VIOLET='\033[35m'
 CYAN='\033[36m'
 GREY='\033[37m'
 
-color=('\033[34m' '\033[37m' '\033[01;31m')
+color=('\033[34m' '\033[37m' '\033[01;31m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m' '\033[00m')
 
-TIMELINE='2022-10-13T10'
+#TIMELINE='2022-10-13T10'
+TIMELINE='2022-10-10T1'
 ETCD_NS='openshift-etcd'
 MUST_PATH=$1
 ORIG_PATH=$(pwd)
@@ -83,27 +84,88 @@ cd $MUST_PATH
 cd $(echo */)
 cd namespaces/$ETCD_NS/pods
 
-overload_check() {
+etcd_check() {
     i=0
     
     for member in $(ls |grep -v "revision"|grep -v "quorum"); do
       echo "processing $member"
       echo -e "" > $OUTPUT_PATH/$member.log
       cat $member/etcd/etcd/logs/current.log |grep "$TIMELINE"|grep 'overloaded'|cut -d ' ' -f1| \
-        xargs -I {} echo -e "{} OVERLOADED     $member !!!" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
-      cat $member/etcd/etcd/logs/current.log |grep "$TIMELINE"|grep 'took too long'|cut -d ' ' -f1| \
-        xargs -I {} echo -e "{} took too long  $member" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
+        xargs -I {} echo -e "{} OVERLOADED     [$member] !!!" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
+      # cat $member/etcd/etcd/logs/current.log |grep "$TIMELINE"|grep 'took too long'|cut -d ' ' -f1| \
+        # xargs -I {} echo -e "{} took too long  [$member]" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
       cat $member/etcd/etcd/logs/current.log |grep "$TIMELINE"|grep 'leader'|cut -d ' ' -f1| \
-        xargs -I {} echo -e "{} LEADER changed $member !" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
+        xargs -I {} echo -e "{} LEADER changed [$member] !" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
       cat $member/etcd/etcd/logs/current.log |grep "$TIMELINE"|grep 'clock'|cut -d ' ' -f1| \
-        xargs -I {} echo -e "{} NTP clock difference $member !!" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
+        xargs -I {} echo -e "{} NTP clock difference [$member] !!" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
+      cat $member/etcd/etcd/logs/current.log |grep "$TIMELINE"|grep 'buffer'|cut -d ' ' -f1| \
+        xargs -I {} echo -e "{} BUFF [$member] !!" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$member.log; done
       #increment color
       i=$((${i}+1))
     done
     i=0
-    cat $OUTPUT_PATH/etcd*.log > $OUTPUT_PATH/logs.log
-    sort -t:  -k2 -k3 $OUTPUT_PATH/logs.log
+    cat $OUTPUT_PATH/etcd*.log > $OUTPUT_PATH/output_etcd_logs.log
+    sort -t:  -k2 -k3 $OUTPUT_PATH/output_etcd_logs.log > $OUTPUT_PATH/sorted.tmp
+    cat $OUTPUT_PATH/sorted.tmp > $OUTPUT_PATH/output_etcd_logs.log
+}
+
+router_check() {
+    i=0
+    
+    for router in $(ls); do
+      echo "processing $router"
+      echo -e "" > $OUTPUT_PATH/$router.log
+      cat $router/router/router/logs/current.log |grep "$TIMELINE"|grep 'Unexpected watch close'|cut -d ' ' -f1| \
+        xargs -I {} echo -e "{} Unexpected watch close     [$router] !!!" | while read -r line; do echo -e "$YELLOW$line$NONE" >> $OUTPUT_PATH/$router.log; done
+      # cat $member/router/router/logs/current.log |grep "$TIMELINE"|grep 'took too long'|cut -d ' ' -f1| \
+        # xargs -I {} echo -e "{} took too long  [$router]" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$router.log; done
+      # cat $member/router/router/logs/current.log |grep "$TIMELINE"|grep 'leader'|cut -d ' ' -f1| \
+      #   xargs -I {} echo -e "{} LEADER changed [$router] !" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$router.log; done
+      # cat $member/router/router/logs/current.log |grep "$TIMELINE"|grep 'clock'|cut -d ' ' -f1| \
+      #   xargs -I {} echo -e "{} NTP clock difference [$router] !!" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$router.log; done
+      # cat $member/router/router/logs/current.log |grep "$TIMELINE"|grep 'buffer'|cut -d ' ' -f1| \
+      #   xargs -I {} echo -e "{} BUFF [$router] !!" | while read -r line; do echo -e "${color[$i]}$line$NONE" >> $OUTPUT_PATH/$router.log; done
+      #increment color
+      i=$((${i}+1))
+    done
+    i=0
+    cat $OUTPUT_PATH/router*.log > $OUTPUT_PATH/output_router_logs.log
+    sort -t:  -k2 -k3 $OUTPUT_PATH/output_router_logs.log > $OUTPUT_PATH/sorted.tmp
+    cat $OUTPUT_PATH/sorted.tmp > $OUTPUT_PATH/output_router_logs.log
+}
+
+stackinfra_check() {
+    i=0
+    
+    for keepalived in $(ls |grep master|grep -v "coredns"|grep -v "haproxy"); do
+      echo "processing $keepalived"
+      echo -e "" > $OUTPUT_PATH/$keepalived.log
+      cat $keepalived/keepalived/keepalived/logs/current.log |grep "$TIMELINE"|grep 'Entering'| \
+        xargs -I {} echo -e "{}      [$keepalived] !!!" | while read -r line; do echo -e "$YELLOW$line$NONE" >> $OUTPUT_PATH/$keepalived.log; done
+      #increment color
+      i=$((${i}+1))
+    done
+    i=0
+    cat $OUTPUT_PATH/keepalived*.log > $OUTPUT_PATH/output_keepalived_logs.log
+    sort -t:  -k2 -k3 $OUTPUT_PATH/output_keepalived_logs.log > $OUTPUT_PATH/sorted.tmp
+    cat $OUTPUT_PATH/sorted.tmp > $OUTPUT_PATH/output_keepalived_logs.log
 }
 
 
-overload_check
+etcd_check
+
+cd ../../..
+cd namespaces/openshift-ingress/pods
+
+router_check
+
+cd ../../..
+cd namespaces/openshift-openstack-infra/pods
+
+stackinfra_check
+
+clear
+
+cd $OUTPUT_PATH
+cat $OUTPUT_PATH/output_keepalived_logs.log $OUTPUT_PATH/output_router_logs.log $OUTPUT_PATH/output_etcd_logs.log > $OUTPUT_PATH/output_logs.log
+sort -tT -k2 -k3 $OUTPUT_PATH/output_logs.log
